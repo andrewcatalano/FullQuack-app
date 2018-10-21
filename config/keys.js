44c9c6345f3c2aba849000940b150e6f0f9150dc
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-21 19:13:57 -0400

    completed validation through vid 15

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jonathan:jonathan3@ds137703.mlab.com:37703/devconnector_new'
+  mongoURI: 'mongodb://jonathan:jonathan3@ds137703.mlab.com:37703/devconnector_new',
+  secretOrKey: 'secret'
 }
