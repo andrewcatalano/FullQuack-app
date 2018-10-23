0180d6731c6b54ed4b2e053534461a3b8bd69a50
filmboy3 <jonathanschwartz30@gmail.com>
2018-10-23 15:36:48 -0400

    fixed validation on Register bug

--- validation/register.js
+++ validation/register.js
@@ -6,7 +6,6 @@ module.exports = function validateRegisterInput(data) {
   data.name = !isEmpty(data.name) ? data.name : '';
   data.email = !isEmpty(data.email) ? data.email : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   if (!Validator.isLength(data.name, {
       min: 2,
@@ -36,14 +35,6 @@ module.exports = function validateRegisterInput(data) {
       max: 30
     })) {
     errors.password = 'Password must be at least 6 characters';
-  }
-
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password field is required';
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Passwords must match';
   }
 
   return {
